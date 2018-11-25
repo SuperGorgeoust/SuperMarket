9b02158d644a050a72092da7f7d718900f2b1ea1
1494915610 <1494915610@qq.com>
2018-11-25 20:45:18 +0800

    type修改

--- src/com/market/bean/Type.java
+++ src/com/market/bean/Type.java
@@ -2,19 +2,18 @@ package com.market.bean;
 
 public class Type {
 	private Long typeId;
-	private String typeName;
+	//大类型得id
 	private Long isMain;
+	//类型级别
+	private String stypeId;
+	//小类型得id
+	private String typeName;
+	//类型名
 	public Long getTypeId() {
 		return typeId;
 	}
 	public void setTypeId(Long typeId) {
 		this.typeId = typeId;
-	}
-	public String getTypeName() {
-		return typeName;
-	}
-	public void setTypeName(String typeName) {
-		this.typeName = typeName;
 	}
 	public Long getIsMain() {
 		return isMain;
@@ -22,9 +21,26 @@ public class Type {
 	public void setIsMain(Long isMain) {
 		this.isMain = isMain;
 	}
+	public String getStypeId() {
+		return stypeId;
+	}
+	public void setStypeId(String stypeId) {
+		this.stypeId = stypeId;
+	}
+	public String getTypeName() {
+		return typeName;
+	}
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
 	@Override
 	public String toString() {
-		return "Type [typeId=" + typeId + ", typeName=" + typeName + ", isMain=" + isMain + "]";
+		return "Type [typeId=" + typeId + ", isMain=" + isMain + ", stypeId=" + stypeId + ", typeName=" + typeName
+				+ "]";
 	}
+	
+	
+	
+	
 	
 }
